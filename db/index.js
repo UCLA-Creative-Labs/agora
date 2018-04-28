287a60b79ca7e18a945f6055c202f032b9d9d325
Bryan Wong <bryanw0ng@ucla.edu>
2018-04-28 01:31:13 -0700

    add readme, remove some postgres config stuff in node server

--- db/index.js
+++ db/index.js
@@ -1,15 +1,13 @@
 const pg = require('pg');
 const pool = new pg.Pool({
-user: 'bryanwong',
 host: '127.0.0.1',
 database: 'cl_db',
-password: '123',
-port: '5432'});
+});
 
 const query = async (text, params) => {
 	try {
-		await pool.connect()
-		const res = await pool.query(text, params)
+		const client = await pool.connect()
+		const res = await client.query(text, params)
 		return res
 	} catch(err) {
 		throw err
