95e2504b7882dd79530c6bc083ff0883f822b63e
Michael Yu <michael.yu2016@gmail.com>
2018-05-24 21:51:23 -0700

    added todos

--- routes/login.js
+++ routes/login.js
@@ -17,11 +17,12 @@ const JwtStrategy = passportJWT.Strategy;
 
 var jwtOptions = {}
 jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-jwtOptions.secretOrKey = 'tasmanianDevil';
+jwtOptions.secretOrKey = 'gigantic_cock';
 
 const strategy = new JwtStrategy(jwtOptions, async (jwt_payload, next) => {
   console.log('payload received', jwt_payload);
-  // usually this would be a database call:
+
+
   const user = users[_.findIndex(users, {id: jwt_payload.id})];
   const user2 = await db.query('SElECT * FROM admins WHERE id=$1', [jwt_payload.id]);
 
@@ -39,30 +40,6 @@ const strategy = new JwtStrategy(jwtOptions, async (jwt_payload, next) => {
   }
 });
 
-/*
-const login = req.body
-	const params = [ login.username ]
-	const admin = await db.query('SElECT * FROM admins WHERE username=$1', params)
-	let payload = {}
-
-	if(!admin.rowCount){
-		payload.err = 'Admin not found.'
-		res.status(404).send(payload)
-	}
-
-	const rows = admin.rows
-	const hash = rows[0].password
-	await bcrypt.compare(login.password, hash, function(err, resp) {
-	  if(resp) {	
-	  	payload.admin = rows[0]
-	  	res.status(200).send(payload)
-	  } else {
-	   	payload.err = 'Password doesn\'t match.'
-	   	res.status(404).send(payload)
-	  } 
-	});
-*/
-
 router.post('/', async (req, res) => {
 
   if(req.body.username && req.body.password){
@@ -76,9 +53,10 @@ router.post('/', async (req, res) => {
   }
 
   const user = admin_query.rows[0];
-  console.log(user);
+
+  // TODO: use bcrypt to check passwords
   if(user.password === req.body.password) {
-    // from now on we'll identify the user by the id and the id is the only personalized value that goes into our token
+    // only identify with id from now on
     var payload = {id: user.id};
     var token = jwt.sign(payload, jwtOptions.secretOrKey);
     res.json({message: "ok", token: token});
@@ -87,6 +65,7 @@ router.post('/', async (req, res) => {
   }
 });
 
+//example endpoint that needs authentication/authorization
 router.get('/secret', passport.authenticate('jwt', { session: false }), async (req, res) => {
   res.json("Success! You can not see this without a token");
 });
