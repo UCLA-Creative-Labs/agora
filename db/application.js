32ec671c7654e8a7ca93de0394846f76a58db623
Bryan Wong <bryanw0ng@ucla.edu>
2018-10-03 22:38:31 -0700

    change table for new fields

--- db/application.js
+++ db/application.js
@@ -1,58 +1,105 @@
-const db = require('./index');
+const db = require("./index");
 
 class Applications {
+  fetchApplications({
+    years,
+    firstChoice,
+    secondChoice,
+    thirdChoice,
+    limit = 400,
+    offset = 0
+  }) {
+    const params = [];
+    const SQLStrings = [];
+    let SQLString = "SELECT * FROM apps";
+    let filterCount = 0;
 
-    fetchApplications({years, firstChoice, secondChoice, thirdChoice, limit = 400, offset = 0}) {
-        const params = [];
-        const SQLStrings = [];
-        let SQLString = 'SELECT * FROM apps';
-        let filterCount = 0;
-
-        if (years) {
-            params.push(strings().toArray(years, Number));
-            SQLStrings.push(`year=ANY($${++filterCount}::int[])`);
-        }
-
-        if (firstChoice) {
-            params.push(firstChoice);
-            SQLStrings.push(`first_choice=$${++filterCount}`);
-        }
-
-        if (secondChoice) {
-            params.push(secondChoice);
-            SQLStrings.push(`second_choice=$${++filterCount}`);
-        }
-
-        if (thirdChoice) {
-            params.push(thirdChoice);
-            SQLStrings.push(`third_choice=$${++filterCount}`);
-        }
-
-        if (SQLStrings.length) {
-            SQLString += ' WHERE ';
-            SQLString += SQLStrings.join(' AND ');
-        }
-
-        params.push(limit);
-        params.push(offset);
-
-        SQLString += ` ORDER BY id`;
-        SQLString += ` LIMIT $${++filterCount}`;
-        SQLString += ` OFFSET $${++filterCount}`;
-
-        return db.query(SQLString, params);
+    if (years) {
+      params.push(strings().toArray(years, Number));
+      SQLStrings.push(`year=ANY($${++filterCount}::int[])`);
     }
 
-    createApplications({firstName, lastName, year, email, response, firstChoice}) {
-        if (!firstName | !lastName | !year | !email | !response | !firstChoice) {
-            throw new Error("Missing Field.");
-        }
-
-        const params = [lastName, firstName, email, 
-                        response, year, firstChoice];
-
-        return db.query('INSERT INTO apps (last_name, first_name, email, response, year, first_choice) VALUES ($1, $2, $3, $4, $5, $6)', params);
+    if (firstChoice) {
+      params.push(firstChoice);
+      SQLStrings.push(`first_choice=$${++filterCount}`);
     }
+
+    if (secondChoice) {
+      params.push(secondChoice);
+      SQLStrings.push(`second_choice=$${++filterCount}`);
+    }
+
+    if (thirdChoice) {
+      params.push(thirdChoice);
+      SQLStrings.push(`third_choice=$${++filterCount}`);
+    }
+
+    if (SQLStrings.length) {
+      SQLString += " WHERE ";
+      SQLString += SQLStrings.join(" AND ");
+    }
+
+    params.push(limit);
+    params.push(offset);
+
+    SQLString += ` ORDER BY id`;
+    SQLString += ` LIMIT $${++filterCount}`;
+    SQLString += ` OFFSET $${++filterCount}`;
+
+    return db.query(SQLString, params);
+  }
+
+  createApplications({
+    name,
+    email,
+    year,
+    pastProject,
+    firstChoiceProject,
+    secondChoiceProject,
+    thirdChoiceProject,
+    whyFirstChoice,
+    whySecondChoice,
+    whyThirdChoice,
+    seeCreativeLabs,
+    links,
+    creativity
+  }) {
+    const params = [
+      name,
+      email,
+      year,
+      pastProject,
+      firstChoiceProject,
+      secondChoiceProject,
+      thirdChoiceProject,
+      whyFirstChoice,
+      whySecondChoice,
+      whyThirdChoice,
+      seeCreativeLabs,
+      links,
+      creativity
+    ];
+
+    return db.query(
+      "INSERT INTO apps (\
+        name, \
+        email, \
+        year, \
+        past_project, \
+        first_choice, \
+        second_choice, \
+        third_choice, \
+        why_first_choice, \
+        why_second_choice, \
+        why_third_choice, \
+        see_creative_labs, \
+        links, \
+        creativity \
+       ) \
+       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)",
+      params
+    );
+  }
 }
 
 const ApplicationsInstance = new Applications();
